5d76186829f258924d71523b878daca03aafc31a
Mohammed Ajmal Siddiqui <ajmalsiddiqui21@gmail.com>
2018-02-25 00:46:14 +0530

    Added production DB creds

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	db_url: process.env.NODE_ENV === 'development' ? 'mongodb://127.0.0.1:27017/students' : 'mongodb://admin:admin@ds249128.mlab.com:49128/hackoverflow',
-	port: '8000',
+	port: '8000' || process.env.PORT,
 	enableCors: true
 }
